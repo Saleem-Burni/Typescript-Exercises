6613cb5df6429c7bf22ddf3451aac5764ec678c8
Saleem-Burni <161769929+Saleem-Burni@users.noreply.github.com>
2024-03-01 23:14:57 +0500

    Add files via upload

---
+++ Ex. 6 Stripping Name/Stripping_Name.ts
@@ -0,0 +1,9 @@
+const personName :string = '    \n\tSaleem Uddin\t\n    ';
+
+console.log(personName);
+
+
+const without_whitespace :string = personName.trim();
+console.log(without_whitespace);
+
+
