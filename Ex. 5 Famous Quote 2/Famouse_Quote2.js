e661f7e020545c7df4a390315812bd7b359948f3
Saleem-Burni <161769929+Saleem-Burni@users.noreply.github.com>
2024-03-01 23:13:05 +0500

    Add files via upload

---
+++ Ex. 5 Famous Quote 2/Famouse_Quote2.js
@@ -0,0 +1,4 @@
+"use strict";
+let famouse_person = 'Albert Einstein';
+let message = '“A person who never made a mistake.”';
+console.log(`${famouse_person} once said, ${message}`);
---
+++ Ex. 5 Famous Quote 2/Famouse_Quote2.ts
@@ -0,0 +1,4 @@
+let famouse_person :string = 'Albert Einstein';
+let message :string = '“A person who never made a mistake.”';
+
+console.log(`${famouse_person} once said, ${message}`);
