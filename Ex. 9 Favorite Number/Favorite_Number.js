06345b7cb6481a191aa3e2720db2aa9f0109cd72
Saleem-Burni <161769929+Saleem-Burni@users.noreply.github.com>
2024-03-01 23:20:24 +0500

    Add files via upload

---
+++ Ex. 9 Favorite Number/Favorite_Number.js
@@ -0,0 +1,4 @@
+"use strict";
+const favorite_number = 7;
+const message = 'This is my favorite number ' + favorite_number;
+console.log(message);
---
+++ Ex. 9 Favorite Number/Favorite_Number.ts
@@ -0,0 +1,5 @@
+const favorite_number :number = 7  ;
+const message :string = 'This is my favorite number ' + favorite_number ;
+
+console.log(message);
+
